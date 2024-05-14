8bb9f048180e9db64803e0ecde47604fcaa54bec
anchisap <anchisap@ssi-steel.com>
2024-05-14 15:08:49 +0700

    Sharepoint

--- project.py
+++ project.py
@@ -1,8 +1,26 @@
-from Sharepoint import SharePoint
+from sharepoint import SharePoint  # นำเข้า SharePoint จากไฟล์หรือโมดูลที่คุณสร้าง
 from openpyxl import Workbook
 
-#get CL1_S1007_Absence_Request sharepoint list
-CL1_S1007_Absence_Request = SharePoint().connect_to_list(ls_name='CL1_S1007_Absence_Request')
+# get CL1_S1007_Absence_Request sharepoint list
+CL1_S1007_Absence_Requests = SharePoint().connect_to_list(list_name='CL1_S1007_Absence_Request')  # แก้ไขชื่อพารามิเตอร์เป็น list_name
+
+# create excel workbook
+wb = Workbook()
+
+dest_filepath = 'CL1_S1007_Absence_Request_list.xlsx'
+
+# create worksheet
+ws = wb.active
+ws.title = 'CL1_S1007_Absence_Request'
+
+# setting SharePoint list values to excel cells
+for idx, request in enumerate(CL1_S1007_Absence_Requests, 1):  # แก้ไขชื่อในลูปให้ไม่ซ้ำกัน
+    ws.cell(column=1, row=idx, value=request['Title'])  # แก้ไขชื่อฟิลด์เป็น request
+    ws.cell(column=2, row=idx, value=request['AddressInfo: Street'])  # แก้ไขชื่อฟิลด์ให้ถูกต้อง
+    ws.cell(column=3, row=idx, value=request['AddressInfo: City'])  # แก้ไขพารามิเตอร์ column
+
+# save workbook
+wb.save(filename=dest_filepath)
 
 
-print(CL1_S1007_Absence_Request)
+
--- sharepoint.py
+++ sharepoint.py
@@ -1,46 +1,42 @@
 from shareplum import Site, office365
 from shareplum.site import Version
-
-
 import json
 import os
 
-
+# ปรับเส้นทางของไฟล์ config ให้ทำงานได้กับทุกแพลตฟอร์ม
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-config_path = '\\'.join([ROOT_DIR, 'confif.json'])
+config_path = os.path.join(ROOT_DIR, 'config.json')
 
-
-#read json config file 
-with open(config_path ) as config_file:
-    config = json.load(cofig_file)
+# อ่านไฟล์ config.json
+with open(config_path, 'r') as config_file:
+    config = json.load(config_file)  # แก้ไขการพิมพ์ผิดจาก cofig_file เป็น config_file
     config = config['share_point']
-
 
 USERNAME = config['user']
 PASSWORD = config['password']
 SHAREPOINT_URL = config['url']
 SHAREPOINT_SITE = config['site']
 
-
-
-
 class SharePoint:
     def auth(self):
         self.authcookie = office365(
-        SHAREPOINT_URL,
-        username = USERNAME,
-        password=PASSWORD,
-    ).GetCookies()
+            SHAREPOINT_URL,
+            username=USERNAME,
+            password=PASSWORD,
+        ).GetCookies()
         self.site = Site(
             SHAREPOINT_SITE,
-            Version=Version.365,
+            version=Version.v365,  # แก้ไขการเขียน version ให้ถูกต้องตามการใช้งานของ shareplum
             authcookie=self.authcookie,
         )
         return self.site
     
-    def connect_to_list(self, ls_name):
+    def connect_to_list(self, list_name):  # เปลี่ยนจาก is_name เป็น list_name
         self.auth_site = self.auth()
-
-        list_data = self.auth_site.List(list_name=is_name).GetListItems()
-
+        list_data = self.auth_site.List(list_name=list_name).GetListItems()
         return list_data
+
+# ตัวอย่างการใช้งาน:
+# sp = SharePoint()
+# data = sp.connect_to_list('ชื่อของลิสต์')
+# print(data)
